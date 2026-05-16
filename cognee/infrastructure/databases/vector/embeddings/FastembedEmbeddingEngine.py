e97d0bab2ede8e7cda76b15534e5c2752faf11df
Igor Ilic <igorilic03@gmail.com>
2026-05-16 15:43:04 +0200

    fix: allow fastembed retry

--- cognee/infrastructure/databases/vector/embeddings/FastembedEmbeddingEngine.py
+++ cognee/infrastructure/databases/vector/embeddings/FastembedEmbeddingEngine.py
@@ -85,7 +85,7 @@ class FastembedEmbeddingEngine(EmbeddingEngine):
     @retry(
         stop=stop_after_delay(128),
         wait=wait_exponential_jitter(8, 128),
-        retry=retry_if_not_exception_type((litellm.exceptions.NotFoundError, EmbeddingException)),
+        retry=retry_if_not_exception_type((litellm.exceptions.NotFoundError)),
         before_sleep=before_sleep_log(logger, logging.WARNING),
         reraise=True,
     )
